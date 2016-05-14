b6fc8f50924f9cde162ad0a5c45d85afdf94995e
alvin <alvin@olemedia.com>
2016-05-14 17:17:30 -0500

    auto generate id by timestamp

--- enter-spark.sh
+++ enter-spark.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-ID=$1
+ID=`date +%Y%m%d-%H%M%S`
 docker run -it --rm --env-file=./hadoop.env --net hadoop --name spark-hadoop-${ID} --volume $(pwd):/data/ awesomedata/hadoop-spark bash
