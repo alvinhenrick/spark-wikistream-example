043d2c18cd135eb12ff7e5ea7906997708abecd6
alvin <alvin@olemedia.com>
2016-05-21 10:42:45 -0500

    fix consumer

--- start-producer.sh
+++ start-producer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/opt/spark-1.6.1/bin/spark-submit --master spark://spark-master.hadoop:7077 --class com.wiki.produce.WikiProduceStream --deploy-mode client --driver-memory 4g --executor-memory 2g --executor-cores 2 target/spark-wikistream-example.jar
+/opt/spark-1.6.1/bin/spark-submit --master spark://spark-master.hadoop:7077 --class com.wiki.produce.WikiProduceStream --deploy-mode client --driver-memory 1g --executor-memory 1g --executor-cores 1 target/spark-wikistream-example.jar
